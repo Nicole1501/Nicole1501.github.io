14ef788c5cf3ad3f036b9bb184cd0fcd73124965
Nicole <nikole150102@hotmail.com>
2026-03-31 08:35:53 -0400

    Update keylogger.js #9

---
+++ keylogger.js
@@ -0,0 +1,28 @@
+let keyCounts = {};
+
+document.addEventListener("keydown", function(event) {
+   let key = event.key;
+
+   if (keyCounts[key]) {
+      keyCounts[key]++;
+   } else {
+      keyCounts[key] = 1;
+   }
+
+   console.clear();
+   for (let k in keyCounts) {
+      console.log(k + ": " + keyCounts[k]);
+   }
+   
+   if (key === " " && keyCounts[key] === 5) {
+      document.body.style.backgroundColor = "lightblue";
+   }
+   
+   if (key === "a" && keyCounts[key] === 10) {
+      document.body.style.color = "red";
+   }
+
+   if (key === "Enter" && keyCounts[key] === 3) {
+      alert("You pressed Enter 3 times!");
+   }
+});
