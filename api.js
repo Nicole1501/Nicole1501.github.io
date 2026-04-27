ed929c85f1d786d3d9eb2c118767eb9803236928
Nicole <nikole150102@hotmail.com>
2026-04-27 16:21:24 -0400

    changes in api.html and api.js #11

--- api.js
+++ api.js
@@ -1,19 +1,15 @@
 function getQuotes() {
   let count = document.getElementById("quoteCount").value;
 
+  document.getElementById("results").innerHTML = "";
+
   let output = "";
 
   for (let i = 0; i < count; i++) {
-    fetch("https://api.quotable.io/random")
+    fetch("https://api.adviceslip.com/advice?timestamp=" + new Date().getTime())
       .then(response => response.json())
       .then(data => {
-        output += `
-          <p>
-            "${data.content}" <br>
-            <strong>- ${data.author}</strong>
-          </p>
-        `;
-
+        output += `<p>"${data.slip.advice}"</p>`;
         document.getElementById("results").innerHTML = output;
       })
       .catch(error => {
